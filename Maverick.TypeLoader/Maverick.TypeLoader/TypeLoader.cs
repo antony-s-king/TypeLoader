ad8d006dabbb4d2fa76a234db3b6d233aa4e9363
Antony King <antony.king@fphcare.co.nz>
2016-01-20 07:45:25 +1300

    First commit to master branch

---
+++ Maverick.TypeLoader/Maverick.TypeLoader/TypeLoader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Maverick.TypeLoader
+{
+    public class TypeLoader<T> where T: class
+    {
+        public T LoadImplementingTypeFromFiles(IEnumerable<string> fileNames, string typeName)
+        {
+            var implementingTypes = FindImplementingTypesInMultipleFiles(fileNames);
+            var typeFound = SelectTypeFromListFound(typeName, implementingTypes);
+            return (T)Activator.CreateInstance(typeFound.Assembly.FullName, typeFound.FullName).Unwrap();
+        }
+
+        private static Type SelectTypeFromListFound(string typeName, IEnumerable<Type> implementingTypes)
+        {
+            var typeFound = implementingTypes.First(t => t.Name == typeName);
+            return typeFound;
+        }
+
+        private IEnumerable<Type> FindImplementingTypesInMultipleFiles(IEnumerable<string> fileNames)
+        {
+            var implementingTypes = new List<Type>();
+            foreach (var file in fileNames)
+            {
+                var typesFound = FindImplementingTypesInSingleFile(file).ToArray();
+                implementingTypes.AddRange(typesFound);
+            }
+            return implementingTypes;
+        }
+
+        private IEnumerable<Type> FindImplementingTypesInSingleFile(string assemblyFileName)
+        {
+            return Assembly.LoadFile(assemblyFileName)
+                           .GetExportedTypes()
+                           .Where(t => typeof(T).IsAssignableFrom(t));
+        }
+    }
+}
